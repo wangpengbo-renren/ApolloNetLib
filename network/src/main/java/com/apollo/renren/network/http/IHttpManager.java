568ca350ca9cd9ab35d73a9d0b61ea909ad711d4
Pengbo Wang <pengbo.wang@renren-inc.com>
2018-09-26 15:07:19 +0800

    interface

---
+++ network/src/main/java/com/apollo/renren/network/http/IHttpManager.java
@@ -0,0 +1,22 @@
+package com.apollo.renren.network.http;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.ArrayMap;
+
+import com.apollo.renren.network.response.Callback;
+
+/**
+ * 该接口用于定义网络框架的基本协议，如Get、Post请求
+ * 不管用什么第三方网络库，封装第三方库的类都要实现这个接口
+ * 如使用Okhttp，则创建OkhttpManager，实现IHttpManager
+ */
+public interface IHttpManager {
+    <T> void doGet(@NonNull String url, @Nullable ArrayMap<String, Object> params, @Nullable Callback<T> callback);
+
+    <T> void doPost(@NonNull String url, @Nullable ArrayMap<String, Object> params, @Nullable Callback<T> callback);
+
+    void cancelRequestByTag(Object tag);
+
+    void cacelAllRequests();
+}
---
+++ network/src/main/java/com/apollo/renren/network/response/Callback.java
@@ -0,0 +1,4 @@
+package com.apollo.renren.network.response;
+
+public interface Callback<T> {
+}
